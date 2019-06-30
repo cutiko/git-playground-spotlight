d50bf782a2ba0475e5b871558c8c6fff77a682ce
cutiko <cutiko@gmail.com>
2019-06-29 21:15:57 -0400

    ADD: more info on links

--- src/components/ItemPr.vue
+++ src/components/ItemPr.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="flex flex-row">
+  <div class="flex flex-row max-w-md">
     <img class="rounded-full avatar" :src="pr.user.avatar_url">
-    <p class="m-2">
-      {{pr.user.login}}
-    </p>
-    <a :href="pr.pull_request.html_url" target="_blank" rel="noreferrer" class="m-2">{{pr.closed_at | formatDate}}</a>
+    <a :href="pr.user.login | githubLink" target="_blank" rel="noreferrer" class="mx-4 py-2 flex-grow">{{pr.user.login}}</a>
+    <a :href="pr.pull_request.html_url" target="_blank" rel="noreferrer" class="py-2">{{pr.closed_at | formatDate}}</a>
   </div>
 </template>
 
@@ -19,7 +17,9 @@
       pr: {type: Object as ()=> PullRequest},
     },
     filters: {
-      formatDate: (uglyDate: string)=>moment(uglyDate).format("MMMM d YYYY")
+      formatDate: (uglyDate: string)=>moment(uglyDate).format("MMMM d YYYY"),
+      githubLink: (user:string)=>`https://github.com/${user}`
+
     }
   });
 
@@ -27,7 +27,7 @@
 
 <style>
   .avatar {
-    width: 2em;
-    height: 2em;
+    width: 3rem;
+    height: 3rem;
   }
 </style>
