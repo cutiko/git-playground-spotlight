ca2d4ef145f7bd7f85e2ae314db633892049e39e
cutiko <cutiko@gmail.com>
2019-06-29 21:46:39 -0400

    REMOVE: hello world

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app" class="container mx-auto">
-    <HelloWorld message="Hello World Cutiko"/>
     <PrList/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 import PrList from "@/components/ListPr.vue";
 
 
 export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld,
     PrList
   }
 });
