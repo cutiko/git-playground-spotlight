24ebc6079451732a142b1b7205d2102678b280a7
cutiko <cutiko@gmail.com>
2019-06-29 21:18:11 -0400

    ADD: increase margin

--- src/components/ItemPr.vue
+++ src/components/ItemPr.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row max-w-md">
     <img class="rounded-full avatar" :src="pr.user.avatar_url">
-    <a :href="pr.user.login | githubLink" target="_blank" rel="noreferrer" class="mx-4 py-2 flex-grow">{{pr.user.login}}</a>
+    <a :href="pr.user.login | githubLink" target="_blank" rel="noreferrer" class="mx-6 py-2 flex-grow">{{pr.user.login}}</a>
     <a :href="pr.pull_request.html_url" target="_blank" rel="noreferrer" class="py-2">{{pr.closed_at | formatDate}}</a>
   </div>
 </template>
