aef8233e937ea6d1e1d28f31b026034ae5b5e8c5
cutiko <cutiko@gmail.com>
2019-06-29 22:29:02 -0400

    ADD: FOOTer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="fixed bottom-0 bg-purple-500 clearfix w-full p-4">
+    <p class="text-center text-white">Share with your friends</p>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  //TODO twitter share //http://twitter.com/share?text=Im%20Sharing%20on%20Twitter&url=https://stackoverflow.com/users/2943186/youssef-subehi&hashtags=stackoverflow,example,youssefusf
+  export default Vue.extend({
+    name: "Footer"
+  })
+</script>
