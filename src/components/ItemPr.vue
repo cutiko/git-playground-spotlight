48363e70b0f7e1185c84349f1aa9ae426f6589de
cutiko <cutiko@gmail.com>
2019-06-29 21:09:12 -0400

    ADD: basic format to the pr row

--- src/components/ItemPr.vue
+++ src/components/ItemPr.vue
@@ -1,27 +1,33 @@
 <template>
-  <div>
-    <img :src="pr.user.avatar_url">
-    <p>
+  <div class="flex flex-row">
+    <img class="rounded-full avatar" :src="pr.user.avatar_url">
+    <p class="m-2">
       {{pr.user.login}}
     </p>
-    <p>{{pr.closed_at}}</p>
-    <p>{{pr.pull_request.html_url}}</p>
+    <a :href="pr.pull_request.html_url" target="_blank" rel="noreferrer" class="m-2">{{pr.closed_at | formatDate}}</a>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
   import {PullRequest} from "@/models/PullRequest";
+  import moment from "moment"
 
   export default Vue.extend({
     name: 'ItemPr',
     props: {
       pr: {type: Object as ()=> PullRequest},
     },
+    filters: {
+      formatDate: (uglyDate: string)=>moment(uglyDate).format("MMMM d YYYY")
+    }
   });
 
 </script>
 
-<style scoped>
-
+<style>
+  .avatar {
+    width: 2em;
+    height: 2em;
+  }
 </style>
