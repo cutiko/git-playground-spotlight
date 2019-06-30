a737f32b5c313518aae79cf525de297c8c175346
cutiko <cutiko@gmail.com>
2019-06-29 22:10:02 -0400

    ADD: explanation

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
     <Header/>
+    <Explanation/>
     <PrList/>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import PrList from "@/components/ListPr.vue";
-import Header from "@/components/Header.vue";
+  import Vue from 'vue';
+  import PrList from "@/components/ListPr.vue";
+  import Header from "@/components/Header.vue";
+  import Explanation from "@/components/Explanation.vue";
 
 
-export default Vue.extend({
+  export default Vue.extend({
   name: 'app',
   components: {
     PrList,
     Header,
+    Explanation,
   }
 });
 </script>
---
+++ src/components/Explanation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto p-4 mb-2">
+    <p class="text-gray-700 text-justify tracking-wide leading-relaxed">Open Source is wonderful, contributing to projects help us all. It doesn't mather if you are a seasoned veteran or if this is
+    your first day coding, submitting a pull request is always a situation bound to your emotions. What should I write? Did I double check everything? Am I missing typo?</p>
+    <p class="text-gray-700 text-justify tracking-wide leading-relaxed">There is a repo that can help you with that
+      <a href="https://github.com/kapunahelewong/git-playground" target="_blank" rel="noreferrer" class="underline text-blue-400">
+      git-playground</a>, go there and play around, learn by doing.</p>
+    <h2 class="text-center p-6 text-lg">This are persons just like you who dare to make their <b>first pull request</b>, go ahead and earn your place on the spotlight</h2>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: "Explanation"
+  })
+</script>
