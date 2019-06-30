334d2d489bc9c256b4fc53ec1a911bbd07528b6c
cutiko <cutiko@gmail.com>
2019-06-29 21:54:03 -0400

    ADD: header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
-  <p>header</p>
+  <div class="bg-purple-600 p-6 mb-4">
+    <h1 class="text-white text-center text-3xl">git-playground Spotlight</h1>
+    <p class="text-white text-center text-lg mt-2">Brave People</p>
+  </div>
 </template>
 
 <script lang="ts">
