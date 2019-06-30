608ba697cbc6ea43806cbdd61cfb8e814fd96c93
cutiko <cutiko@gmail.com>
2019-06-29 21:48:54 -0400

    ADD: header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app" class="container mx-auto">
+  <div id="app">
+    <Header/>
     <PrList/>
   </div>
 </template>
@@ -7,12 +8,14 @@
 <script lang="ts">
 import Vue from 'vue';
 import PrList from "@/components/ListPr.vue";
+import Header from "@/components/Header.vue";
 
 
 export default Vue.extend({
   name: 'app',
   components: {
-    PrList
+    PrList,
+    Header,
   }
 });
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>header</p>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({
+    name: "Header"
+  })
+</script>
+
+<style scoped>
+
+</style>
